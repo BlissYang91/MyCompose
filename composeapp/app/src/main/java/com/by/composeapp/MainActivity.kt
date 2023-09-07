4dc510c1418261907b800b9d59e7b23584ada55a
yangtianfu <2812420513@qq.com>
2023-09-07 17:31:06 +0800

    AsyncImage

--- composeapp/app/src/main/java/com/by/composeapp/MainActivity.kt
+++ composeapp/app/src/main/java/com/by/composeapp/MainActivity.kt
@@ -5,36 +5,36 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.remember
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.by.composeapp.ui.theme.ComposeAppTheme
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,6 +46,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    // TextField
+                    ShowTextField()
 //                    Greeting("Android")
 //                    Column(modifier = Modifier.verticalScroll(rememberScrollState())) { //数据量很大的时候性能可能会非常低
 //                        for (i in 0 ..20) {
@@ -67,12 +69,12 @@ class MainActivity : ComponentActivity() {
 //                    )
 
 
-                    LazyColumn() {
-                        items(getData()) { data ->
-                            More(title = "$data--")
-                        }
-                    }
-//
+//                    LazyColumn() {
+//                        items(getData()) { data ->
+//                            More(title = "$data--")
+//                        }
+//                    }
+
 //                    LazyRow() {
 //                        items(getData()) { data ->
 //                            More(title = "$data--")
@@ -88,6 +90,49 @@ class MainActivity : ComponentActivity() {
     }
 
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShowTextField() {
+    Column() {
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            placeholder = {
+                Text(text = "hint text")
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                placeholderColor = Color.Blue,
+                cursorColor = Color.Red,
+                textColor = Color.Yellow
+            )
+        )
+
+        Image(
+            // image 设置尺寸和圆形加载
+            modifier = Modifier
+                .width(50.dp)
+                .height(50.dp)
+                .clip(CircleShape),
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "This is a example image"
+        )
+
+        val imageBitmap = ImageBitmap.imageResource(id = R.drawable.biying)
+        Image(bitmap = imageBitmap, contentDescription = null)
+
+        AsyncImage(
+            model = "https://img-blog.csdnimg.cn/20200401094829557.jpg",
+            contentDescription = "this is a coil image",
+            placeholder = painterResource(id = R.drawable.ic_launcher_background)
+        )
+
+
+    }
+
+}
+
 
 @Composable
 fun Counter() {
@@ -127,7 +172,7 @@ fun More(title: String) {
     }
     // 动画状态
     val extraPadding by animateDpAsState(
-        if(expanded) 48.dp else 0.dp
+        if (expanded) 48.dp else 0.dp
     )
 
     Column(modifier = Modifier.padding(bottom = extraPadding)) {
@@ -139,16 +184,20 @@ fun More(title: String) {
             Text(text = title, fontSize = 16.sp, color = Color.Blue, modifier = Modifier.weight(1f))
             Button(onClick = {
                 expanded = !expanded
-                Toast.makeText(context, if (!expanded) "查看详情" else "收起", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, if (!expanded) "查看详情" else "收起", Toast.LENGTH_SHORT)
+                    .show()
             }) {
                 Text(text = if (!expanded) "查看详情" else "收起", color = Color.White)
             }
 
         }
         if (expanded) {
-            Text(text = "详情页展示。。。。。", modifier = Modifier.fillMaxSize()
-                .height(80.dp)
-                .background(Color.Yellow))
+            Text(
+                text = "详情页展示。。。。。", modifier = Modifier
+                    .fillMaxSize()
+                    .height(80.dp)
+                    .background(Color.Yellow)
+            )
         }
     }
 }
